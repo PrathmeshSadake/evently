5fbed9a201e90d5b9ec19e5d886662f4e375c360
Prathmesh Sadake <prathmeshsadake@gmail.com>
2024-01-03 22:53:55 +0530

    Added user actions

--- src/api/webhook/clerk/route.ts
+++ src/api/webhook/clerk/route.ts
@@ -1,7 +1,7 @@
 import { Webhook } from "svix";
 import { headers } from "next/headers";
 import { WebhookEvent } from "@clerk/nextjs/server";
-import { createUser } from "@/lib/actions/user.actions";
+import { createUser, deleteUser, updateUser } from "@/lib/actions/user.actions";
 import { clerkClient } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
@@ -56,7 +56,7 @@ export async function POST(req: Request) {
   const eventType = evt.type;
 
   if (eventType === "user.created") {
-    const { id, email_addresses, first_name, last_name, image_url, username } =
+    const { id, email_addresses, image_url, first_name, last_name, username } =
       evt.data;
 
     const user = {
@@ -67,6 +67,7 @@ export async function POST(req: Request) {
       lastName: last_name,
       photo: image_url,
     };
+
     const newUser = await createUser(user);
 
     if (newUser) {
@@ -80,28 +81,28 @@ export async function POST(req: Request) {
     return NextResponse.json({ message: "OK", user: newUser });
   }
 
-  //   if (eventType === "user.updated") {
-  //     const { id, image_url, first_name, last_name, username } = evt.data;
+  if (eventType === "user.updated") {
+    const { id, image_url, first_name, last_name, username } = evt.data;
 
-  //     const user = {
-  //       firstName: first_name,
-  //       lastName: last_name,
-  //       username: username!,
-  //       photo: image_url,
-  //     };
+    const user = {
+      firstName: first_name,
+      lastName: last_name,
+      username: username!,
+      photo: image_url,
+    };
 
-  //     const updatedUser = await updateUser(id, user);
+    const updatedUser = await updateUser(id, user);
 
-  //     return NextResponse.json({ message: "OK", user: updatedUser });
-  //   }
+    return NextResponse.json({ message: "OK", user: updatedUser });
+  }
 
-  //   if (eventType === "user.deleted") {
-  //     const { id } = evt.data;
+  if (eventType === "user.deleted") {
+    const { id } = evt.data;
 
-  //     const deletedUser = await deleteUser(id!);
+    const deletedUser = await deleteUser(id!);
 
-  //     return NextResponse.json({ message: "OK", user: deletedUser });
-  //   }
+    return NextResponse.json({ message: "OK", user: deletedUser });
+  }
 
   return new Response("", { status: 200 });
 }
